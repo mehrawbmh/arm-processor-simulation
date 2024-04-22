badc225ce383cd7030049609828912935300992a
Mehrab <mehrabmh81@yahoo.com>
2024-04-22 18:51:15 +0330

    fix: make adder combinational

--- IF/IF_adder.v
+++ IF/IF_adder.v
@@ -1,12 +1,3 @@
 module IF_adder(input clk,rst,input [31:0] pcOut,output reg [31:0] PC);
-
-    always@(posedge clk,posedge rst)begin
-        if (rst) begin
-            PC = 32'd0;
-        end
-        else begin
-            PC <= pcOut+32'd4;
-        end
-
-    end
+    assign PC = pcOut + 32'd4;
 endmodule
