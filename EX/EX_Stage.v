dc53325dc16ced0106637f513738a8c531de2c9a
Mehrab <mehrabmh81@yahoo.com>
2024-04-22 18:58:32 +0330

    fix imm32 shifting and wiring

--- EX/EX_Stage.v
+++ EX/EX_Stage.v
@@ -16,8 +16,8 @@ module EX_Stage (
 ); 
     wire [31:0] val2;
     
-    wire imm32;
-    assign imm32 = sigend_imm_24[23] ? {8'b1, shift_operand} : {8'd0, shift_operand};
+    wire [31:0] imm32;
+    assign imm32 = sigend_imm_24[23] ? {8'b1, sigend_imm_24} : {8'd0, sigend_imm_24};
 
     val2_generator valGen(val_rm, imm,shift_operand,mem_R_en, mem_W_en, val2);
     ALU alu (val_rn, val2, status_reg[1], exe_command, alu_result, status);
