abc660e19d230b5bf01ec33a5f00d1212181b810
Mehrab <mehrabmh81@yahoo.com>
2024-04-22 18:55:56 +0330

    feat: add status signals to ALU

--- EX/ALU.v
+++ EX/ALU.v
@@ -3,7 +3,8 @@ module ALU (
     input[31:0] val2,
     input carry,
     input [3:0] exe_command,
-    output reg result
+    output reg [31:0] result,
+    output [3:0] status
 );
 
     assign carry_bar = !carry;
@@ -21,8 +22,11 @@ module ALU (
             4'b1000: result = val1 ^ val2;
             default: result = 32'dx;
         endcase
-
     end
 
-
+    assign status[0] = (val1[31] && val2[31] && !result[31]) || (!val1[31] && !val2[31] && result[31]); //v bit for overflow
+    assign status[1] = status[0]; //c bit forcarry
+    assign status[2] = (result == 32'd0); // z bit for zero detection
+    assign status[3] = result[31]; //n bit for negative result
+    
 endmodule
